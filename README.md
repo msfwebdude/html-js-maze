6b54f5573ffa0bfacda64510f4fe023722243edb
Mike Firoved <msfwebdude@gmail.com>
2023-06-07 18:55:35 -0700

    added rules and updated docs

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-# html-js-maze
-An attempt at creating a maze in javascript and html only
+![GitHub](https://img.shields.io/github/license/msfwebdude/html-js-maze?style=plastic) ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/msfwebdude/html-js-maze?style=plastic)
+![GitHub last commit](https://img.shields.io/github/last-commit/msfwebdude/html-js-maze?style=plastic)
+
+# HTML and Javascript Maxe
+An attempt at creating a maze in javascript and html only. 
 
 
-## progress
+## Rules
+* top, bottom, left, and right rows must have outer borders
+* corners must have both closed sides bordered
+* single cells that do not connect should not be allowed (*not yet implemented*)
+* a path should exist between start and finish (*not yet implemented*)
+
+## Cell design
 the cells have 16 cell types
 
 ![Screenshot](assets/img/cell-types.png)
+
+
+## Todo
+* change cells object array to an object that we can reference
+* implement rules not yet implemented (*above*)
+* create a clearly defined entrance and exit to the maze
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,56 @@
-var spaces = []
+var cells = []
 
 var arena_width  = 64;
 var arena_height = 64;
 
 
 function loadArena(){
-    var arena_contents = "";
+  var arena_contents = "";
 
+  for (let y = 0; y < arena_height; y++) {
     for (let x = 0; x < arena_width; x++) {
-        spaces[x] = [];
-        for (let y = 0; y < arena_height; y++) {
-            var div_id = ("00000000" + Math.round((Math.random() * 10000000)).toString(36)).toUpperCase().substr(-5);
-            var wall_type = Math.floor((Math.random() * 0xF));
-            var wall_css  = `cellType${wall_type.toString(16).toUpperCase()}`
-            spaces[x][y]  = {
-                div_id,
-                wall_type
-            };
-            arena_contents += `<span id="${div_id}" class="cellCommon ${wall_css}">&nbsp;</span>`;
-        }
-        arena_contents += `<br />`;
-    }
-    
-    self.arena.innerHTML = arena_contents;
-    self.arena.style.width = arena_width * 16;
+        var div_id = ("00000000" + Math.round((Math.random() * 10000000)).toString(36)).toUpperCase().substr(-5);
+        
+        var allowed = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE]
+        
+        // outer bounds walls
+        if (x == 0)                allowed = [2, 3, 5, 6, 9, 0xA, 0xB]
+        if (y == 0)                allowed = [1, 3, 4, 6, 7, 0xA, 0xC]
+        if (x + 1 == arena_width)  allowed = [2, 3, 4, 5, 7, 8, 0xD]
+        if (y + 1 == arena_height) allowed = [1, 4, 5, 6, 8, 9, 0xE]
+        
+        // corners
+        if (x == 0 && y == 0)                               allowed = [3, 6, 0xA]   // top left corner
+        if (x == 0 && y + 1 == arena_height)                allowed = [5, 6, 9]     // bottom left corner
+        if (y == 0 && x + 1 == arena_width)                 allowed = [3, 4, 7]     // top right corner
+        if (y + 1 == arena_height && x + 1 == arena_width)  allowed = [4, 5, 8]     // bottom right corner
+
+        var wall_type = pickOne(allowed)
+        var wall_css  = `cellType${wall_type.toString(16).toUpperCase()}`
+        cells.push(
+          {
+            x,
+            y,
+            div_id,
+            wall_type
+          }
+        );
+        arena_contents += `<span id="${div_id}" date-coords="${x}, ${y}" class="cellCommon ${wall_css}">&nbsp;</span>`;
+      }
+      arena_contents += `<br />`;
+  }
+  
+  self.arena.innerHTML = arena_contents;
+  self.arena.style.width = arena_width * 16;
+}
+
+function pickOne(array){
+  if (Array.isArray(array)){
+    var l = array.length;
+    var i = Math.floor(Math.random() * l)
+    return array[i];
+  }
+  else return null;
 }
 
 
